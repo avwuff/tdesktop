ac49af720b8fc9133a91980e352b2f1c301e576a
Av <github@avbrand.com>
2020-03-30 19:50:21 -0700

    Update info_media_inner_widget.cpp
    Updated to TG 2.0

--- Telegram/SourceFiles/info/media/info_media_inner_widget.cpp
+++ Telegram/SourceFiles/info/media/info_media_inner_widget.cpp
@@ -121,7 +121,7 @@ void InnerWidget::createTypeButtons() {
 
 	addMediaButton(Type::Photo, st::infoIconMediaPhoto);
 	addMediaButton(Type::Video, st::infoIconMediaVideo);
-	addMediaButton(Type::GIF, st::infoIconMediaGIF);
+	addMediaButton(Type::GIF, st::infoIconMediaVideo);
 	addMediaButton(Type::File, st::infoIconMediaFile);
 	addMediaButton(Type::MusicFile, st::infoIconMediaAudio);
 	addMediaButton(Type::Link, st::infoIconMediaLink);
